c459a8e24817186c99c0d4ea8a364383537add78
Erick Mejía <ericknmejia@gmail.com>
2025-05-20 09:39:57 +0200

    feat: add primary button

---
+++ resources/js/components/BtnPrimary.vue
@@ -0,0 +1,25 @@
+<!-- BtnPrimary.vue -->
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: 'submit',
+    },
+    processing: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<template>
+    <button
+        :type="type"
+        :disabled="processing"
+        class="btn btn-primary w-full"
+        :class="processing ? 'btn-disabled cursor-not-allowed' : ''"
+    >
+        <slot v-if="!processing" />
+        <span v-else>Procesando...</span>
+    </button>
+</template>
