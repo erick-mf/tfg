505815fb496943f4e411dba3477a2c7d6bcdf41e
Erick Mejía <ericknmejia@gmail.com>
2025-05-20 09:38:36 +0200

    feat: add login page

---
+++ resources/js/layouts/AuthLayout.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="flex min-h-screen flex-col">
+        <main class="flex flex-1 items-center justify-center p-4">
+            <slot />
+        </main>
+    </div>
+</template>
---
+++ resources/js/pages/Login.vue
@@ -0,0 +1,52 @@
+<script setup>
+import BtnPrimary from '@/components/BtnPrimary.vue';
+import AuthLayout from '@/layouts/AuthLayout.vue';
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    password: '',
+});
+
+function submit() {
+    if (form.password.length !== 6) {
+        form.setError('password', 'La contraseña no es correcta');
+    } else {
+        form.post(route('dashboard'));
+    }
+}
+</script>
+
+<template>
+    <AuthLayout>
+        <div class="mx-auto w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
+            <div class="w-full rounded-lg bg-white p-8 shadow-xl">
+                <div class="mb-8 text-center">
+                    <a :href="route('home')" class="text-3xl font-semibold">Logo</a>
+                </div>
+
+                <form @submit.prevent="submit" class="flex flex-col gap-2">
+                    <div class="min-h-[20px]">
+                        <p v-if="form.errors.password" class="text-sm text-red-600">
+                            {{ form.errors.password }}
+                        </p>
+                    </div>
+
+                    <div class="flex flex-col gap-4">
+                        <label class="input w-full text-lg">
+                            <input
+                                v-model="form.password"
+                                type="password"
+                                placeholder="Contraseña de acceso"
+                                autocomplete="off"
+                            />
+                        </label>
+
+                        <div>
+                            <BtnPrimary type="submit" :processing="form.processing"> Ingresar </BtnPrimary>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </AuthLayout>
+</template>
