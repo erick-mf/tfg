db101e2dcdf92911300b6c4023325e683efd3cd1
Erick Mejía <ericknmejia@gmail.com>
2025-05-27 10:50:12 +0200

    refactor(style): menu item component

--- resources/js/components/MenuItem.vue
+++ resources/js/components/MenuItem.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import { CircleChevronRight } from 'lucide-vue-next';
 
 defineProps({
     href: {
@@ -10,30 +11,14 @@ defineProps({
         type: Boolean,
         default: false,
     },
-    icon: {
-        type: String,
-        required: true,
-    },
 });
 </script>
 
 <template>
     <li>
         <Link :href="href" :class="['flex items-center', active ? 'bg-primary/10 text-primary' : 'hover:bg-base-200']">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                :class="{
-                    'stroke-primary': active,
-                    'stroke-current': !active,
-                }"
-            >
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" :d="icon" />
-            </svg>
-            <span class="ml-2 text-base"><slot /></span>
+            <CircleChevronRight :class="['h-5 w-5', active ? 'block' : 'hidden']" />
+            <span class="text-base"><slot /></span>
         </Link>
     </li>
 </template>
